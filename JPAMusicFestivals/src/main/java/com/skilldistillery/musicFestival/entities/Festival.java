22c938a25fbe0d16a9d6da4943ac23f95c21f3d4
Chris Lee <91095529+Clee350z@users.noreply.github.com>
2022-01-07 15:35:01 -0700

    Built Festival

---
+++ JPAMusicFestivals/src/main/java/com/skilldistillery/musicFestival/entities/Festival.java
@@ -0,0 +1,60 @@
+package com.skilldistillery.musicFestival.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Festival {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Festival() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Festival other = (Festival) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Festival [id=" + id + ", name=" + name + "]";
+	}
+}
---
+++ JPAMusicFestivals/src/test/java/com/skilldistillery/musicFestival/entities/FestivalTest.java
@@ -0,0 +1,54 @@
+package com.skilldistillery.musicFestival.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FestivalTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Festival festival;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAMusicFestivals");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		festival = em.find(Festival.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		festival = null;
+	}
+
+
+	
+	@Test
+	void test_Festival_mapping() {
+	assertNotNull(festival);
+	assertEquals("Global Dance", festival.getName());;
+	
+	}
+
+}
